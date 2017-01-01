93cce01f2adeb9739553b3ebf9a4e7ce0b26323a
Gregory Shilin <gshilin@gmail.com>
2017-01-01 12:37:26 +0200

    new statistics + many improvements

--- bb-bakara.go
+++ bb-bakara.go
@@ -11,6 +11,7 @@ import (
 	"flag"
 	"github.com/rach/pome/Godeps/_workspace/src/github.com/jmoiron/sqlx"
 	"github.com/tealeg/xlsx"
+	"strconv"
 )
 
 type persons_type map[string]int
@@ -22,171 +23,310 @@ var tables = dbStruct{
 	//"acc_levelset": []string{"id", "level_name"},
 	//"acc_levelset_door_group": []string{"id", "acclevelset_id", "accdoor_id", "accdoor_no_exp", "accdoor_device_id"},
 	//"acc_levelset_emp": []string{"id", "acclevelset_id", "employee_id"},
-	"acc_monitor_log": []string{"id", "time", "pin", "card_no", "device_id", "device_sn", "device_name", "event_point_name"},
 	//"DEPARTMENTS": []string{"id", "DEPTNAME", "SUPDEPTID"},
 	//"Machines": []string{"id", "MachineAlias", "IP", "SerialPort", "Port", "Baudrate", "usercount", "FirmwareVersion",
 	//	"sn", "device_name", "subnet_mask", "gateway", "area_id", "acpanel_type"},
 	//"personnel_area": []string{"id", "areaid", "areaname", "parent_id"},
 	//"personnel_issuecard": []string{"id", "create_time", "UserID_id", "cardno", },
-	"USERINFO": []string{"id", "USERID", "Badgenumber", "name", "Gender", "BIRTHDAY", "CardNo", "lastname", "identitycard", "bankcode1", },
+	"acc_monitor_log": []string{"id", "time", "pin", "card_no", "device_id", "device_sn", "device_name", "event_point_name"},
+	"USERINFO":        []string{"userid", "USERID", "Badgenumber", "name", "Gender", "BIRTHDAY", "CardNo", "lastname", "identitycard", "bankcode1", },
 }
 
 type Config struct {
-	dbAdapter    string
-	connString   string
-	sqlQuery     string
-	limitQuery   string
-	outputFile   string
-	mdbPath      string
-	doNotLoadMdb bool
+	dbAdapter      string
+	connString     string
+	sqlQuery       string
+	limitQuery     string
+	outputPath     string
+	mdbPath        string
+	doNotCalculate bool
+	doNotLoadMdb   bool
+	onlyLoadMdb    bool
+	makor          string
+	noMakor        bool
+	month          int
+	year           int
 }
 
 func main() {
 	cfg := getConfig()
 
-	if !cfg.doNotLoadMdb {
-		loadDB(cfg.mdbPath, tables, cfg)
+	db := openDb(cfg.dbAdapter, cfg.connString)
+	defer db.Close()
+
+	// Prices *HAVE* to be loaded *BEFORE* db
+	if !cfg.noMakor {
+		loadPrices(db, cfg)
 	}
 
-	calculateMoney(cfg)
+	if !cfg.doNotLoadMdb {
+		loadDB(cfg.mdbPath, tables, db, cfg)
+		if cfg.onlyLoadMdb {
+			os.Exit(0)
+		}
+	}
+
+	if !cfg.doNotCalculate {
+		calculateMoney(db, cfg)
+		calculateTotalsPerMeal(db, cfg)
+		statistics(db, cfg)
+	}
+}
+
+type controller struct {
+	chipName   string
+	controller string
+}
+
+var controllerMap = map[string]*controller{
+	"חדר אוכל": {
+		controller: "ארועים",
+		chipName:   "חדר אוכל קטן", },
+	"רחבה סעודות":{
+		controller: "ארועים",
+		chipName:   "רחבת סעודות 1", },
+	"רמפה":{
+		controller: "ארועים",
+		chipName:   "ק.0 מדרגות גימס סעודות", },
+	"סעודות עולם אירועים מערב":{
+		controller: "חדר אמנון פעיל קומה ",
+		chipName:   "ק.0 ארועים מערב סעודות", },
+}
+
+var daysMap = map[string]int{
+	"א'": 0,
+	"ב'": 1,
+	"ג'": 2,
+	"ד'": 3,
+	"ה'": 4,
+	"ו'": 5,
+	"ש'": 6,
+}
+
+func loadPrices(db *sqlx.DB, cfg *Config) {
+	printCommand("loadPrices", "")
+	xlFile, err := xlsx.OpenFile(cfg.makor)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	db.Exec("DROP TABLE IF EXISTS prices;")
+	db.Exec(`
+		CREATE TABLE prices (
+			day INTEGER,
+			dow INTEGER,
+			income TEXT,
+			meal TEXT,
+			start TEXT,
+			finish TEXT,
+			controller TEXT,
+			chip_name TEXT,
+			price INTEGER,
+			vegetarian INTEGER
+		)`)
+	sheet := xlFile.Sheets[0]
+	for _, row := range sheet.Rows {
+		cells := row.Cells
+		//fmt.Println(cells)
+		if len(cells) < 11 || cells[1].Value == "" {
+			continue
+		}
+		if _, err = strconv.Atoi(cells[1].Value); err != nil {
+			continue
+		}
+
+		day, _ := strconv.Atoi(cells[1].Value)
+		dow, ok := daysMap[cells[2].Value]
+		if !ok {
+			fmt.Errorf("### Unknown DOW: %d\n", cells[2].Value)
+			os.Exit(-1)
+		}
+		s := strings.Split(cells[5].Value, "-")
+		start, end := strings.Replace(s[0], ".", ":", -1), strings.Replace(s[1], ".", ":", -1)
+		priceChip, _ := strconv.Atoi(cells[7].Value)
+		priceVegetarian, _ := strconv.Atoi(cells[9].Value)
+		label := strings.TrimSpace(strings.Replace(cells[4].Value, "'", "׳", -1))
+		income := strings.TrimSpace(strings.Replace(cells[3].Value, "'", "׳", -1))
+		for _, chipName := range strings.Split(cells[6].Value, ";") {
+			controller, ok := controllerMap[strings.TrimSpace(chipName)]
+			if !ok {
+				fmt.Errorf("### Unable to find chip: %s\n", chipName)
+				os.Exit(-1)
+			}
+			query := fmt.Sprintf(`
+				INSERT INTO prices (day, dow, income, meal, start, finish, controller, chip_name, price, vegetarian)
+				VALUES(%d, %d, '%s', '%s', '%s', '%s', '%s', '%s', %d, %d);
+			`, day, dow, income, label, start, end, controller.controller, controller.chipName, priceChip, priceVegetarian)
+			if _, err = db.Exec(query); err != nil {
+				log.Fatal(err)
+				os.Exit(-1)
+			}
+		}
+	}
 }
 
 func getConfig() *Config {
 
 	thisYear, thisMonth, _ := time.Now().Date()
+
 	var doNotLoadMdb = flag.Bool("x", false, "Do not reload MDB file, use the existing data")
+	var onlyLoadMdb = flag.Bool("X", false, "ONLY reload MDB file")
 	var mdbPath = flag.String("m", "ZKAccess.mdb", "Path to an MDB file")
-	var outputPath = flag.String("o", "", "Output file path and name ** mandatory **")
+	var outputPath = flag.String("o", "", "** mandatory ** Output file path")
 	var month = flag.Int("d", int(thisMonth - 1), "Month to create report for")
 	var year = flag.Int("y", int(thisYear), "Year to create report for")
 	var pUser = flag.String("u", "postgres", "Postgres User")
 	var pPassword = flag.String("p", "postgres", "Postgres Password")
 	var pHost = flag.String("h", "localhost", "Postgres host")
+	var makor = flag.String("i", "", "File with prices")
+	var noMakor = flag.Bool("I", false, "Do not load file with prices")
+	var doNotCalculate = flag.Bool("C", false, "Do not perform calculations")
+
+	if *month == 0 {
+		*month = 12
+	}
+	if *month == 12 {
+		*year --
+	}
 
 	flag.Parse()
 
-	if *outputPath == "" {
+	if *outputPath == "" && *onlyLoadMdb == false {
 		flag.PrintDefaults()
 		log.Fatal("Please supply all mandatory parameters")
 	}
+
 	if *month <= 0 || *month > 12 {
 		log.Fatal("Bad month number", *month)
 	}
 
-	if (*pPassword == "") {
+	if *pPassword == "" {
 		*pPassword = ""
 	} else {
 		*pPassword = ":" + *pPassword
 	}
 
 	limitQ := fmt.Sprintf(`
-		DELETE FROM acc_monitor_log a
-		WHERE   time < '%d-%02d-01 00:00:00' OR time >= '%d-%02d-01 00:00:00';
-	`, *year, *month, *year, *month + 1)
+		DELETE FROM acc_monitor_log WHERE time < '%d-%02d-01 00:00:00' OR time >= '%d-%02d-01 00:00:00';
+	`, *year, *month, *year, *month+1)
 
 	query := fmt.Sprintf(`
-CREATE EXTENSION IF NOT EXISTS tablefunc;
 
-WITH crosstab AS (
-	SELECT * FROM crosstab(
-		$$ SELECT * FROM (
-			WITH records AS (
-				SELECT
-					pin || ',' || card_no AS name,
-					time, device_name, event_point_name,
-					extract(dow from time)::integer AS dow,
-					to_char(time, 'HH24:MI') AS hm,
+		WITH _in AS (
+			SELECT * FROM all_records WHERE meal IN ('סעודת בוקר', 'סעודת צהריים')
+		),
+		_not_in AS (
+			SELECT DISTINCT * FROM all_records WHERE meal NOT IN ('סעודת בוקר', 'סעודת צהריים')
+		),
+		meals AS (
+			SELECT * FROM _in
+			UNION ALL
+			SELECT * FROM _not_in
+		)
+		SELECT  name "שם",
+			lastname "שם משפחה",
+			'' "טלפון",
+			ophone "מס כרטיס בהנה""ח",
+			income "הכנסה",
+			meal "סוג סעודה",
+			'' "פסצקה",
+			case when pager='1' then vegetarian else price end "מחיר",
+			day || '.%02d.%d' "תאריך",
+			case when pager='1' then 'כן' else '' end "צמחוני",
+			pin || ',' || card_no "badge/tag",
+			'' "הערות",
+			CASE
+				WHEN device_name = 'ארועים' THEN
 					CASE
-						WHEN device_name = 'חדר אמנון פעיל קומה ' THEN 'amnon'
-						ELSE 'dinner'
-					END AS chip
-				FROM acc_monitor_log a
-				WHERE   time >= '%d-%02d-01 00:00:00' AND time < '%d-%02d-01 00:00:00'
-					and (
-						(device_name = 'חדר אמנון פעיל קומה ' and event_point_name = 'ק.1 טכנאי מחשב חדר אמנון')
-						or
-						(device_name = 'גלריה וכניסה' and event_point_name = 'ארוחת צהרים חדר אוכל')
-					)
-			)
-			/* dinner room counter */
-			SELECT name, 'breakfast' AS title, count(1) * 3 AS shekel FROM records
-			WHERE chip = 'dinner' AND dow BETWEEN 0 AND 4 AND hm BETWEEN '02:00' AND '11:30' GROUP BY name
-				UNION
-			SELECT name, 'dinner' AS title, count(1) * 14 AS shekel FROM records
-			WHERE chip = 'dinner' AND dow BETWEEN 0 AND 4 AND hm BETWEEN '11:31' AND '18:00' GROUP BY name
-				UNION
-			SELECT name, 'houmus' AS title, count(1) * 5 AS shekel FROM records
-			WHERE chip = 'dinner' AND dow = 5 AND hm BETWEEN '02:00' AND '11:00' GROUP BY name
-				UNION
-			/* amnon room counter */
-			SELECT name, 'breakfast' AS title, count(1) * 3 AS shekel FROM records
-			WHERE chip = 'amnon' AND dow BETWEEN 0 AND 4 AND hm BETWEEN '04:30' AND '07:00' GROUP BY name
-				UNION
-			SELECT name, 'houmus' AS title, count(1) * 5 AS shekel FROM records
-			WHERE chip = 'amnon' AND dow = 5 AND hm BETWEEN '04:30' AND '07:00' GROUP BY name
-		) meals ORDER BY name $$,
-		$$ VALUES ('breakfast'::text), ('dinner'::text), ('houmus'::text) $$
-	) AS (name TEXT, breakfast BIGINT, dinner BIGINT, houmus BIGINT)
-), a AS (
-	SELECT c.name, coalesce(u.name, '') || ' ' || coalesce(u.lastname, '') full_name, u.ophone, breakfast, dinner, houmus, SUM(coalesce(breakfast, 0) + coalesce(dinner, 0) + coalesce(houmus, 0)) AS total
-	FROM crosstab c
-	LEFT OUTER JOIN userinfo u ON (u.badgenumber = split_part(c.name, ',', 1))
-	GROUP BY c.name, breakfast, dinner, houmus, ophone, full_name
-), by_badge AS (
-	SELECT * FROM a WHERE full_name != ' '
-), by_tag AS (
-	SELECT a.name, coalesce(u.name, '') || ' ' || coalesce(u.lastname, '') full_name, a.ophone, breakfast, dinner, houmus, total
-	FROM a
-	LEFT OUTER JOIN userinfo u ON u.cardno = split_part(a.name, ',', 2)
-	WHERE full_name = ' '
-), final AS (
-	SELECT * FROM by_badge UNION SELECT * FROM by_tag
-)
-
-SELECT ophone "מס כרטיס בהנה""ח", name "badge/tag", full_name "שם מלא", breakfast "סעודות בוקר", dinner "סעודות צהריים", houmus "סעודת חומוס", total "סה""כ"
-FROM (
-	 SELECT 0 ordinal, * FROM final
-	UNION
-	 SELECT 1 ordinal, '', '', 'Grand Total', SUM(coalesce(breakfast, 0)), SUM(coalesce(dinner, 0)), SUM(coalesce(houmus, 0)), SUM(coalesce(total, 0)) FROM final
-) t
-ORDER BY ordinal ASC, "badge/tag" ASC;
-		`, *year, *month, *year, *month + 1)
+						WHEN event_point_name = 'חדר אוכל קטן' THEN 'חדר אוכל'
+						WHEN event_point_name = 'רחבת סעודות 1' THEN 'רחבה סעודות'
+						WHEN event_point_name = 'ק.0 מדרגות ג''מס סעודות' THEN 'רמפה'
+					END
+				WHEN device_name = 'חדר אמנון פעיל קומה ' THEN
+					CASE WHEN event_point_name = 'ק.0 ארועים מערב סעודות' THEN 'סעודות עולם אירועים מערב'
+					END
+			END "קולט"
+		FROM meals
+		WHERE meal IS NOT NULL
+		ORDER BY "מס כרטיס בהנה""ח", "שם משפחה", "שם", day;
+		`, *month, *year)
 
 	return &Config{
-		dbAdapter: "postgres",
-		connString: fmt.Sprintf("postgres://%s%s@%s/zkaccess", *pUser, *pPassword, *pHost),
-		sqlQuery: query,
-		limitQuery: limitQ,
-		outputFile: *outputPath,
-		mdbPath: *mdbPath,
-		doNotLoadMdb: *doNotLoadMdb,
+		dbAdapter:      "postgres",
+		connString:     fmt.Sprintf("postgres://%s%s@%s/zkaccess", *pUser, *pPassword, *pHost),
+		sqlQuery:       query,
+		limitQuery:     limitQ,
+		outputPath:     *outputPath,
+		mdbPath:        *mdbPath,
+		doNotLoadMdb:   *doNotLoadMdb,
+		onlyLoadMdb:    *onlyLoadMdb,
+		makor:          *makor,
+		noMakor:        *noMakor,
+		doNotCalculate: *doNotCalculate,
+		month:          *month,
+		year:           *year,
 	}
 }
 
-func loadDB(dbpath string, tables dbStruct, cfg *Config) (err error) {
+func loadDB(dbpath string, tables dbStruct, db *sqlx.DB, cfg *Config) (err error) {
+	printCommand("LoadDB", "")
 
 	for table := range tables {
 		dbTable := strings.ToLower(table)
 
-		if err = runCommand("Create table " + table,
-			"mdb-schema --drop-table -T " + table + " " + dbpath +
-				" postgres | fgrep -v 'ADD CONSTRAINT' | tr '[:upper:]' '[:lower:]' | psql -U postgres -d zkaccess"); err != nil {
-			return
-		}
-		if err = runCommand("Import table " + table,
-			"mdb-export -H -q \\\" -D '%Y-%m-%d %H:%M:%S' " + dbpath + " " + table +
-				"| psql -U postgres -d zkaccess -c 'COPY " + dbTable + " FROM STDIN CSV'"); err != nil {
-			return
-		}
-	}
+		db.Exec("ALTER SEQUENCE " + dbTable + "_" + tables[table][0] + "_seq RESTART WITH 1;")
 
-	db, err := sqlx.Open(cfg.dbAdapter, cfg.connString)
-	if err != nil {
-		log.Fatal(err)
+		if err = runCommand("Create table "+table,
+			"mdb-schema --drop-table -T "+table+" "+dbpath+" postgres | fgrep -v 'ADD CONSTRAINT' | tr '[:upper:]' '[:lower:]' | psql -U postgres -d zkaccess"); err != nil {
+			return
+		}
+		if err = runCommand("Import table "+table,
+			"mdb-export -H -q \\\" -D '%Y-%m-%d %H:%M:%S' "+dbpath+" "+table+"| psql -U postgres -d zkaccess -c 'COPY "+dbTable+" FROM STDIN CSV'"); err != nil {
+			return
+		}
 	}
-	defer db.Close()
 
 	db.Exec(cfg.limitQuery)
+	// keep only records that belong to our events
+	db.Exec("DROP TABLE IF EXISTS events;")
+	into_events := `
+		  SELECT pin, card_no, device_name, event_point_name,
+			extract(dow from time)::integer AS dow,
+			extract(day from time)::integer AS day,
+			to_char(time, 'HH24:MI') AS hm
+		  INTO events
+		  FROM acc_monitor_log
+		  WHERE false
+	`
+	for _, v := range controllerMap {
+		into_events += fmt.Sprintf(` OR (device_name = '%s' AND event_point_name = '%s')`, v.controller, v.chipName)
+	}
+	db.Exec(into_events)
+
+	for_kolia, _ := db.Queryx(`
+		SELECT day, hm, card_no, device_name, event_point_name
+		FROM events e
+		LEFT OUTER JOIN userinfo u ON u.badgenumber = e.pin OR u.cardno = e.card_no
+		WHERE (coalesce(u.name, '') || ' ' || coalesce(u.lastname, '')) = ' '
+	`)
+	file := xlsx.NewFile()
+	defer file.Save(cfg.outputPath + "/for_kolia" + strconv.Itoa(cfg.month) + "-" + strconv.Itoa(cfg.year) + ".xlsx")
+	writeSheet(for_kolia, file)
+
+	db.Exec(`
+		DROP TABLE IF EXISTS all_records;
+		SELECT e.*, u.name, u.lastname, u.ophone, u.fphone, u.pager, p.meal, p.price, p.vegetarian, p.income
+		INTO all_records
+		FROM events e
+		LEFT OUTER JOIN userinfo u ON u.badgenumber = e.pin OR u.cardno = e.card_no
+		LEFT OUTER JOIN prices p ON e.dow = p.dow AND e.day = p.day AND e.hm BETWEEN p.start AND p.finish
+		AND e.device_name = p.controller AND e.event_point_name = p.chip_name
+		WHERE (coalesce(u.name, '') || ' ' || coalesce(u.lastname, '')) != ' ';
+		-- ignore oraat keva
+		DELETE FROM all_records a WHERE a.dow = 5 AND a.fphone = '2' AND a.hm > '15:00';
+		DELETE FROM all_records a WHERE a.dow = 6 AND a.fphone = '2';
+	`)
 
 	return
 }
@@ -220,26 +360,136 @@ func printOutput(outs []byte) {
 	}
 }
 
-func calculateMoney(cfg *Config) {
-
-	db, err := sqlx.Open(cfg.dbAdapter, cfg.connString)
+func calculateTotalsPerMeal(db *sqlx.DB, cfg *Config) {
+	printCommand("calculateTotalsPerMeal", "")
+	results, err := db.Queryx(`
+		WITH _in AS (
+			SELECT * FROM all_records WHERE meal IN ('סעודת בוקר', 'סעודת צהריים')
+		),
+		_not_in AS (
+			SELECT DISTINCT * FROM all_records WHERE meal NOT IN ('סעודת בוקר', 'סעודת צהריים')
+		),
+		meals AS (
+			SELECT * FROM _in
+			UNION ALL
+			SELECT * FROM _not_in
+		),
+		run_int AS (
+			SELECT meal, day, count(1), SUM(CASE WHEN pager = '1' THEN vegetarian ELSE price END)
+			FROM meals
+			GROUP BY meal, day
+			ORDER BY meal, day ASC
+		),
+		run_tot AS (
+			SELECT meal, 0 AS day, count(1), SUM(CASE WHEN pager = '1' THEN vegetarian ELSE price END)
+			FROM meals
+			GROUP BY meal
+			ORDER BY meal
+		)
+		SELECT * FROM run_int
+		UNION ALL
+		SELECT * FROM run_tot
+		ORDER BY meal, day ASC
+	`)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(-1)
 	}
-	defer db.Close()
 
-	results, err := db.Queryx(cfg.sqlQuery)
-
-	if err != nil {
-		log.Fatal(err)
-	}
 	file := xlsx.NewFile()
-	defer file.Save(cfg.outputFile)
+	defer file.Save(cfg.outputPath + "/meals" + strconv.Itoa(cfg.month) + "-" + strconv.Itoa(cfg.year) + ".xlsx")
 
+	writeSheet(results, file)
+
+}
+
+func calculateMoney(db *sqlx.DB, cfg *Config) {
+	printCommand("LcalculateMoney", "")
+	results, err := db.Queryx(cfg.sqlQuery)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	file := xlsx.NewFile()
+	defer file.Save(cfg.outputPath + "/report" + strconv.Itoa(cfg.month) + "-" + strconv.Itoa(cfg.year) + ".xlsx")
+
+	writeSheet(results, file)
+}
+
+func statistics(db *sqlx.DB, cfg *Config) {
+	printCommand("statistics", "")
+	// Statistics per input device
+	results, err := db.Queryx(`
+	WITH a AS (
+	SELECT device_name, event_point_name,
+	CASE
+		WHEN device_name = 'ארועים' THEN
+			CASE
+				WHEN event_point_name = 'חדר אוכל קטן' THEN 'חדר אוכל'
+				WHEN event_point_name = 'רחבת סעודות 1' THEN 'רחבה סעודות'
+				WHEN event_point_name = 'ק.0 מדרגות גימס סעודות' THEN 'רמפה'
+				ELSE device_name || '--' || event_point_name
+			END
+		WHEN device_name = 'חדר אמנון פעיל קומה ' THEN
+			CASE
+				WHEN event_point_name = 'ק.0 ארועים מערב סעודות' THEN 'סעודות עולם אירועים מערב'
+				ELSE device_name || '--' || event_point_name
+			END
+		ELSE device_name || '--' || event_point_name
+	END "device"
+	FROM acc_monitor_log
+	WHERE device_name IN ('ארועים', 'חדר אמנון פעיל קומה ') AND
+	      event_point_name IN ('חדר אוכל קטן', 'רחבת סעודות 1', 'ק.0 מדרגות גימס סעודות', 'ק.0 ארועים מערב סעודות')
+	)
+	SELECT count(device), device, device_name, event_point_name
+	FROM a
+	GROUP BY device, device_name, event_point_name`)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	file := xlsx.NewFile()
+	defer file.Save(cfg.outputPath + "/chip_reader" + strconv.Itoa(cfg.month) + "-" + strconv.Itoa(cfg.year) + ".xlsx")
+
+	writeSheet(results, file)
+
+	// Price per "מס כרטיס בהנה""ח"
+	results, err = db.Queryx(`
+			WITH _in AS (
+				SELECT * FROM all_records WHERE meal IN ('סעודת בוקר', 'סעודת צהריים')
+			),
+			_not_in AS (
+				SELECT DISTINCT * FROM all_records WHERE meal NOT IN ('סעודת בוקר', 'סעודת צהריים')
+			),
+			meals AS (
+				SELECT * FROM _in
+				UNION ALL
+				SELECT * FROM _not_in
+			)
+	select ophone "מס כרטיס בהנה""ח", sum(case when pager = '1' then vegetarian else price end) "מחיר"
+	from meals
+	GROUP BY ophone
+	`)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	file1 := xlsx.NewFile()
+	defer file1.Save(cfg.outputPath + "/totals" + strconv.Itoa(cfg.month) + "-" + strconv.Itoa(cfg.year) + ".xlsx")
+
+	writeSheet(results, file1)
+}
+
+func writeSheet(results *sqlx.Rows, file *xlsx.File) (err error) {
 	sheet, err := file.AddSheet("Sheet1") // col bestFit="1"
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(-1)
 	}
+
 	row := sheet.AddRow()
 	columns, _ := results.Columns()
 	for _, h := range columns {
@@ -251,6 +501,7 @@ func calculateMoney(cfg *Config) {
 		row, err := results.SliceScan()
 		if err != nil {
 			log.Fatal(err)
+			os.Exit(-1)
 		}
 
 		for _, col := range row {
@@ -275,4 +526,18 @@ func calculateMoney(cfg *Config) {
 			}
 		}
 	}
+
+	return
+
+}
+
+func openDb(adapter string, conn string) *sqlx.DB {
+
+	db, err := sqlx.Open(adapter, conn)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	return db
 }
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Sync controllers: "ארועים" and "חדר אמנון פעיל קומה " ;doorkeys; admin/admin
+Copy \\bb-bakaradb\ZKAccess3.5\ZKAccess.mdb
+Run ```go build bb-bakara.go``` to create linux executable
+prices: https://docs.google.com/spreadsheets/d/1UvhtwUb9nl-K_WK_v2dVNbDPScwjE5FwetTEOCeA_7Q/edit#gid=1907896725
+go build bb-bakara.go; ./bb-bakara -i /media/sf_projects/bb-bakaradb/reports/prices12-2016.xlsx -m /media/sf_projects/bb-bakaradb/ZKAccess.mdb -o /media/sf_projects/bb-bakaradb/reports
